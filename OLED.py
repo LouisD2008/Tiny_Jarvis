18272ed2fb82b7283077d9390711690b114d90c7
Louis Denaiffe <louisdenaiffe@gmail.com>
2026-07-13 16:01:21 +0200

    oled gif support v1

--- OLED.py
+++ OLED.py
@@ -1,15 +1,19 @@
 # Note: on the rpi5, do sudo apt install libjpeg-dev zlib1g-dev libfreetype6-dev liblcms2-dev libopenjp2-7-dev libtiff6-dev
 from luma.core.interface.serial import i2c
 from luma.oled.device import ssd1106
-from PIL import Image
+from PIL import Image, ImageSequence # this is for gifs
 import time
 
 
-serial = i2c(port=1, adress=0x3C)
+serial = i2c(port=1, address=0x3C)
 device = ssd1106(serial, width=128, height=64)
 
 
 def oled(file_path):
-    img = Image.open(file_path).convert('1')
-    device.display(img)
-    time.sleep(10)
+    img = Image.open(file_path)
+    if hasattr(img, "is_animated") and img.is_animated:
+        for frame in ImageSequence.Iterator(img):
+            device.display(frame.convert('1'))
+            time.sleep(0.1)
+    else:
+        device.display(img.convert('1'))
--- ai.py
+++ ai.py
@@ -4,11 +4,15 @@ from ollama import ChatResponse
 
 
 def generate(prompt):
-    response: ChatResponse = chat(model='gemma3', messages=[
-        {
+    response: ChatResponse = chat(
+        model='gemma3', 
+        messages=[{
         'role': 'user',
         'content': "Answer in a concise, thoughtful way to the following prompt: " + prompt
-        # stream = true
-        },
-    ])
-    return response.message.content
+        }],
+        stream = True
+    )
+    # return response.message.content
+    # instead of that, we use yield keyword
+    for chunk in response:
+        yield chunk.message.content
